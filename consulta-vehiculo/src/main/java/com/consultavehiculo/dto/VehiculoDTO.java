af100ade419e680854c0b12e51a621569965b745
tom-ayu <tomas.ontaneda@udla.edu.ec>
2025-06-08 20:11:12 -0500

    PruebaVehiculo

--- consulta-vehiculo/src/main/java/com/consultavehiculo/dto/VehiculoDTO.java
+++ consulta-vehiculo/src/main/java/com/consultavehiculo/dto/VehiculoDTO.java
@@ -1,15 +1,30 @@
 package com.consultavehiculo.dto;
 
-import lombok.*;
+import lombok.Data;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class VehiculoDTO {
-    private String placa;
-    private String marca;
-    private String modelo;
-    private String anio;
-    private String tipo;
+    private Integer codigoVehiculo;
+    private String numeroPlaca;
+    private String numeroCamvCpn;
+    private String colorVehiculo1;
+    private String colorVehiculo2;
+    private String cilindraje;
+    private String nombreClase;
+    private String descripcionMarca;
+    private String descripcionModelo;
+    private Integer anioAuto;
+    private String descripcionPais;
+    private String mensajeMotivoAuto;
+    private Boolean aplicaCuota;
+    private String fechaUltimaMatricula;
+    private String fechaCaducidadMatricula;
+    private String fechaCompraRegistro;
+    private String fechaRevision;
+    private String descripcionCanton;
+    private String descripcionServicio;
+    private Integer ultimoAnioPagado;
+    private String prohibidoEnajenar;
+    private String observacion;
+    private String estadoExoneracion;
 }
